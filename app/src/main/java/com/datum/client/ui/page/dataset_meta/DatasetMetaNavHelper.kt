98b649e7f735020d1eb62b2bfbeeeca5948f0de6
Mark Fedyashov <fedyashov@i-teco.ru>
2022-04-14 18:00:51 +0300

    start add dataset meta page

---
+++ app/src/main/java/com/datum/client/ui/page/dataset_meta/DatasetMetaNavHelper.kt
@@ -0,0 +1,37 @@
+package com.datum.client.ui.page.dataset_meta
+
+import androidx.navigation.NamedNavArgument
+import androidx.navigation.NavBackStackEntry
+import androidx.navigation.NavType
+import androidx.navigation.navArgument
+import com.datum.client.dto.DatasetDto
+import com.datum.client.repository.ArgumentRepository
+import com.datum.client.ui.page.NavHelper
+import kotlin.reflect.KClass
+
+class DatasetMetaNavHelper: NavHelper() {
+    private val ID_ARG = "id"
+    override fun getNavArguments(): List<NamedNavArgument> {
+        return listOf(navArgument(ID_ARG){
+            type = NavType.IntType
+        })
+    }
+
+    override fun templateUrl(): String {
+        return "dataset/{$ID_ARG}"
+    }
+
+    override fun substituteArgument(vararg arr: Any): String {
+        val datasetId = arr[0] as Int
+        return "dataset/${datasetId}"
+    }
+
+    override fun typeOf(): KClass<*> {
+        return DatasetMetaPage::class
+    }
+
+    fun getDatasetMeta(b: NavBackStackEntry) : DatasetDto {
+        val id = getArg<Int>(b, ID_ARG)
+        return ArgumentRepository.getArgument(id!!)
+    }
+}
---
+++ app/src/main/java/com/datum/client/ui/page/dataset_meta/DatasetMetaPage.kt
@@ -0,0 +1,4 @@
+package com.datum.client.ui.page.dataset_meta
+
+class DatasetMetaPage {
+}
